33e8af899f9c66abb6c399458f18a61022ebcbdc
Rayhan <rayhanfaz7@gmail.com>
2023-10-22 17:34:32 -0400

    Setup structure for processing the input n

--- aks.py
+++ aks.py
@@ -1,1 +1,11 @@
+import sys
 
+def main(n):
+    """The main function which will run the AKS algorithm on the given input n. It will use
+    helper functions for each of the steps in the algorithm. This function will output
+    either PRIME or COMPOSITE.
+    """
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1]) # Will output PRIME or COMPOSITE
